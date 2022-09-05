2a5223daad34243ebbeeee2c6fa54840e54ad9b2
Tais Bruno <taisbruno@id.uff.br>
2022-09-05 17:04:42 -0300

    Execucao do TDD

--- tdd/test/Processador/Processadortest.java
+++ tdd/test/Processador/Processadortest.java
@@ -2,17 +2,24 @@ package Processador;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class Processadortest {
 	Processador processador;
 
 	@BeforeEach
 	public void incializaProcessador() {
 		processador = new Processador();
+
 	}
 
 	@Test
 	public void testpagarFatura() {
-			}
+		Assertions.assertEquals("boleto", pagarFatura(boleto));
+	}
 
 	@Test
 	public void testverificaFatura() {
